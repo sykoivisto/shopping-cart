1318657f88a215a61294b1e8c46734a6756d684c
sy <sykoivisto@gmail.com>
2023-02-12 20:04:49 -0700

    style changes

--- src/data/itemData.js
+++ src/data/itemData.js
@@ -3,28 +3,28 @@ const itemData = {
     {
       name: 'Core i7, RTX 3080',
       price: 1950,
-      img: `${process.env.PUBLIC_URL}/img/desktop-angled.jpg`,
+      img: `${process.env.PUBLIC_URL}/img/desktoppi.jpg`,
       altImg: `${process.env.PUBLIC_URL}/img/desktop-side.jpg`,
       id: 1000
     },
     {
       name: 'Core i5, RTX 3070',
       price: 1450,
-      img: `${process.env.PUBLIC_URL}/img/desktop-angled.jpg`,
+      img: `${process.env.PUBLIC_URL}/img/desktoppi.jpg`,
       altImg: `${process.env.PUBLIC_URL}/img/desktop-side.jpg`,
       id: 1001
     },
     {
       name: 'Core i5, RTX 3060',
       price: 1250,
-      img: `${process.env.PUBLIC_URL}/img/desktop-angled.jpg`,
+      img: `${process.env.PUBLIC_URL}/img/desktoppi.jpg`,
       altImg: `${process.env.PUBLIC_URL}/img/desktop-side.jpg`,
       id: 1002
     },
     {
       name: 'Core i3, RTX 3050',
       price: 950,
-      img: `${process.env.PUBLIC_URL}/img/desktop-angled.jpg`,
+      img: `${process.env.PUBLIC_URL}/img/desktoppi.jpg`,
       altImg: `${process.env.PUBLIC_URL}/img/desktop-side.jpg`,
       id: 1003
     }
@@ -33,28 +33,28 @@ const itemData = {
     {
       name: '17" Core i7, RTX 3080',
       price: 2075,
-      img: `${process.env.PUBLIC_URL}/img/laptop-angled.jpg`,
+      img: `${process.env.PUBLIC_URL}/img/laptoppi.jpg`,
       altImg: `${process.env.PUBLIC_URL}/img/laptop-front.jpg`,
       id: 1004
     },
     {
       name: '17" Core i5, RTX 3070',
       price: 1575,
-      img: `${process.env.PUBLIC_URL}/img/laptop-angled.jpg`,
+      img: `${process.env.PUBLIC_URL}/img/laptoppi.jpg`,
       altImg: `${process.env.PUBLIC_URL}/img/laptop-front.jpg`,
       id: 1005
     },
     {
       name: '17" Core i5, RTX 3060',
       price: 1275,
-      img: `${process.env.PUBLIC_URL}/img/laptop-angled.jpg`,
+      img: `${process.env.PUBLIC_URL}/img/laptoppi.jpg`,
       altImg: `${process.env.PUBLIC_URL}/img/laptop-front.jpg`,
       id: 1006
     },
     {
       name: '17" Core i3, RTX 3050',
       price: 1075,
-      img: `${process.env.PUBLIC_URL}/img/laptop-angled.jpg`,
+      img: `${process.env.PUBLIC_URL}/img/laptoppi.jpg`,
       altImg: `${process.env.PUBLIC_URL}/img/laptop-front.jpg`,
       id: 1007
     }
@@ -63,28 +63,28 @@ const itemData = {
     {
       name: 'RTX 3080',
       price: 799,
-      img: `${process.env.PUBLIC_URL}/img/rtx-3080-angle.jpg`,
+      img: `${process.env.PUBLIC_URL}/img/3080.jpg`,
       altImg: `${process.env.PUBLIC_URL}/img/rtx-3080-front.jpg`,
       id: 1008
     },
     {
       name: 'RTX 3070',
       price: 499,
-      img: `${process.env.PUBLIC_URL}/img/rtx-3070-angle.jpg`,
+      img: `${process.env.PUBLIC_URL}/img/3070.jpg`,
       altImg: `${process.env.PUBLIC_URL}/img/rtx-3070-front.jpg`,
       id: 1009
     },
     {
       name: 'RTX 3060',
       price: 329,
-      img: `${process.env.PUBLIC_URL}/img/rtx-3060-angle.jpg`,
+      img: `${process.env.PUBLIC_URL}/img/3060.jpg`,
       altImg: `${process.env.PUBLIC_URL}/img/rtx-3060-front.jpg`,
       id: 1010
     },
     {
       name: 'RTX 3050',
       price: 249,
-      img: `${process.env.PUBLIC_URL}/img/rtx-3050-angle.jpg`,
+      img: `${process.env.PUBLIC_URL}/img/3050.jpg`,
       altImg: `${process.env.PUBLIC_URL}/img/rtx-3050-front.jpg`,
       id: 1011
     }
